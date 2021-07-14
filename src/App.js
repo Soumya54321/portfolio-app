5ed1c9bf95f4ab2cfb36e4839451eb1994599607
Soumyadeep Paul <soumyadeeppaul0@gmail.com>
2021-07-14 20:24:19 +0530

    extra package removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import Header from './components/header';
 
